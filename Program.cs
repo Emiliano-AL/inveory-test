f5c46f45433c65e79ff47b8e3bcf59d2c09679e2
Emiliano <emiliano.0218@gmail.com>
2026-03-20 13:36:48 -0600

    setup project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ================================================================
+// ====================== INVENTARIO APP ========================
+// ================================================================
+
+Console.WriteLine("================================================================");
+Console.WriteLine("====================== INVENTARIO APP ========================");
+Console.WriteLine("================================================================");
+
+Console.WriteLine("Ingrese el nombre del producto:");
+// string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese el precio del producto:");
+// float precio = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad del producto:");
